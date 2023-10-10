a37b42f1b78afb4be42ec980dc9024b9c2c8c17a
wingshope <138878860+wingshope@users.noreply.github.com>
2023-10-10 14:50:37 +0700

    Update kyt.sh

--- kyt.sh
+++ kyt.sh
@@ -9,13 +9,13 @@ NC='\e[0m'
 apt update && apt upgrade
 apt install python3 python3-pip git
 cd /usr/bin
-wget https://raw.githubusercontent.com/Hermananza/vip/main/bot/bot.zip
+wget https://raw.githubusercontent.com/wingshope/vip/main/bot/bot.zip
 unzip bot.zip
 mv bot/* /usr/bin
 chmod +x /usr/bin/*
 rm -rf bot.zip
 clear
-wget https://raw.githubusercontent.com/Hermananza/vip/main/bot/kyt.zip
+wget https://raw.githubusercontent.com/wingshope/vip/main/bot/kyt.zip
 unzip kyt.zip
 pip3 install -r kyt/requirements.txt
 
