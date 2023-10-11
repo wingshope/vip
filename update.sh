58eee51736902ae1e1f205aec59e51549bb711ec
wingshope <138878860+wingshope@users.noreply.github.com>
2023-10-11 11:16:37 +0700

    Update update.sh

--- update.sh
+++ update.sh
@@ -41,7 +41,7 @@ res1() {
 netfilter-persistent
 clear
 echo -e "\033[1;36m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e " \e[1;97;101m          UPDATE SCRIPT KYTVPN       \e[0m"
+echo -e " \e[1;97;101m          UPDATE SCRIPT        \e[0m"
 echo -e "\033[1;36m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 echo -e ""
 echo -e "  \033[1;91m update script service\033[1;37m"
